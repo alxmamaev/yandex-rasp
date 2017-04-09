c14fd7132d5dfe410e8e718cc8c6f82bc059c922
AlexanderMamaev <alxmamaev@ya.ru>
2017-04-09 22:27:31 +0500

    Add keyboards

--- modules/schedule.py
+++ modules/schedule.py
@@ -9,7 +9,8 @@ def init(bot):
 
 def start(bot, message):
 	GET_FIRST_STATION = bot.render_message("get-first-station")
-	bot.telegram.send_message(message.u_id, GET_FIRST_STATION)
+	BACK_TO_MENU_KEYBOARD = bot.get_keyboard("back-to-menu")
+	bot.telegram.send_message(message.u_id, GET_FIRST_STATION, reply_markup = BACK_TO_MENU_KEYBOARD)
 
 	bot.user_delete(message.u_id, "station:1")
 	bot.user_delete(message.u_id, "station:2")
@@ -40,6 +41,7 @@ def get_station_name(bot, message):
 
 def select_station(bot, message):
 	GET_SECOND_station = bot.render_message("get-second-station")
+	BACK_TO_MENU_KEYBOARD = bot.get_keyboard("back-to-menu")
 
 	stations_keyboard = bot.user_get(message.u_id, "stations_keyboard")
 	station = bot.get_key(stations_keyboard, message.text)
@@ -52,7 +54,7 @@ def select_station(bot, message):
 
 	if not first_station:
 		bot.user_set(message.u_id, "station:1", station)
-		bot.telegram.send_message(message.u_id, GET_SECOND_station)
+		bot.telegram.send_message(message.u_id, GET_SECOND_station, reply_markup = BACK_TO_MENU_KEYBOARD)
 		bot.set_next_handler(message.u_id, "schedule/get-station-name")
 	else:
 		bot.user_set(message.u_id, "station:2", station)
@@ -62,6 +64,8 @@ def select_station(bot, message):
 
 def search(bot, message):
 	SCHEDULE_IS_EMPTY = bot.render_message("schedule-is-empty")
+	READY = bot.render_message("ready")
+	BACK_TO_MENU_KEYBOARD = bot.get_keyboard("back-to-menu")
 
 	from_station = bot.user_get(message.u_id, "station:1")
 	to_station = bot.user_get(message.u_id, "station:2")
@@ -104,10 +108,11 @@ def search(bot, message):
 		keyboard = None
 
 	if schedule:
+		bot.telegram.send_message(message.u_id, READY, reply_markup = BACK_TO_MENU_KEYBOARD)
 		SCHEDULE = bot.render_message("schedule", schedule = schedule[0:5])
 		bot.telegram.send_message(message.u_id, SCHEDULE, parse_mode = "Markdown", reply_markup = keyboard)
 	else:
-		bot.telegram.send_message(message.u_id, SCHEDULE_IS_EMPTY)
+		bot.telegram.send_message(message.u_id, SCHEDULE_IS_EMPTY, reply_markup = BACK_TO_MENU_KEYBOARD)
 
 def show_shedule(bot, query):
 	page = bot.user_get(message.u_id, "schedule:page")+1
